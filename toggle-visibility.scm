51dd8ba30cb45c9601e68d3738f38519873ab65c
mugwhump <thugwhump@yahoo.com>
2014-07-16 19:25:28 -0700

    Actually finished the script

--- toggle-visibility.scm
+++ toggle-visibility.scm
@@ -1,22 +1,26 @@
 (define (script-fu-toggle-visibility img layer)
-  (gimp-message (car (gimp-image-get-filename img)))
-  (gimp-message "AAAAAAAAAAAAAAA")
+  (let* (
+         (num-layers (car (gimp-image-get-layers img)))
+         (all-layers (cadr (gimp-image-get-layers img)))
+         (toggle-layer (vector-ref all-layers (- num-layers 2)))
+         (is-visible (car (gimp-item-get-visible toggle-layer)))
+         (becomes-visible (- 1 is-visible))
+         )
+
+    (gimp-item-set-visible toggle-layer becomes-visible)
+    )
   )
 
 (script-fu-register
   "script-fu-toggle-visibility"
-  "Outline Text"                        ; Label
-  "Creates a simple outline around\
-  the selected text layer."             ; Description
+  "Toggle Visible"                        ; Label
+  "Toggles visibility of 2nd-from-bottom layer."
   "Grayson Bartlet"
   "Fat Man License"
   "May 2014"
   ""                                    ; Works on all image types
   SF-IMAGE "Image" 0
   SF-DRAWABLE "Drawable" 0
-;  SF-COLOR "Outline Color" '(255 255 255)     ; Default white
-;  SF-VALUE "Outline Width (px)" "3"
-;  SF-TOGGLE "Merge Text With Outline?" TRUE
 )
 
 (script-fu-menu-register "script-fu-toggle-visibility" "<Image>/Layer/Toggle")
