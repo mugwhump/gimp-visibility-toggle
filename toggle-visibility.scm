4902d7c1eb8ab688dddd2e09ec00de9c90af453c
mugwhump <thugwhump@yahoo.com>
2014-07-21 13:18:29 -0700

    Display flush so changes show up.

--- toggle-visibility.scm
+++ toggle-visibility.scm
@@ -5,10 +5,14 @@
          (toggle-layer (vector-ref all-layers (- num-layers 2)))
          (is-visible (car (gimp-item-get-visible toggle-layer)))
          (becomes-visible (- 1 is-visible))
+         (width (car (gimp-drawable-width toggle-layer)))
+         (height (car (gimp-drawable-height toggle-layer)))
          )
 
+    (begin
     (gimp-item-set-visible toggle-layer becomes-visible)
-    )
+    (gimp-displays-flush)
+    ))
   )
 
 (script-fu-register
