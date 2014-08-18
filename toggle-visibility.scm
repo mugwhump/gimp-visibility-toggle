7c4865f2a4c1dd8769141f99bbd6012f2022132a
mugwhump <thugwhump@yahoo.com>
2014-08-18 15:59:54 -0700

    Added macro to define functions for moving scripts in various directions.

--- toggle-visibility.scm
+++ toggle-visibility.scm
@@ -28,3 +28,31 @@
 )
 
 (script-fu-menu-register "script-fu-toggle-visibility" "<Image>/Layer/Toggle")
+
+(macro (define-layer-moving-function form) 
+       (let* (
+              (func-name (cadr form))
+              (x-off (caddr form))
+              (y-off (cadddr form))
+              )
+       `(begin
+        (define (func-name img layer) 
+                (begin
+                  (gimp-layer-translate layer ,x-off ,y-off)
+                  (gimp-displays-flush))) 
+        (script-fu-register
+          (symbol->string ,func-name)
+          "Move da shit"                        ; Label
+          "It moves shit"
+          "Grayson Bartlet"
+          "Fat Man License"
+          "May 2014"
+          ""                                    ; Works on all image types
+          SF-IMAGE "Image" 0
+          SF-DRAWABLE "Drawable" 0
+        )
+
+        (script-fu-menu-register (symbol->string ,func-name) "<Image>/Layer/Toggle")
+)))
+
+(define-layer-moving-function 'script-fu-move-layer-up 0 -10)
